1954ad7e6a8f5898c9588f670b3e439653f192bd
Axizeaus <axizeaus@gmail.com>
2023-08-11 14:22:15 +0630

    starting over from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import { Link } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello from the main page of the app!</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="shop">Shop</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <div>This is App</div>;
 };
 
 export default App;
